db6c71f2644b3c1e9058fdad1fa9408ebf47490b
Wisner Júnior <wisnerjr@gmail.com>
2020-08-19 23:48:21 -0300

    sections 4 and 5 - class components and state

--- components/src/ApprovalCard.js
+++ components/src/ApprovalCard.js
@@ -2,16 +2,16 @@ import React from 'react';
 import CommentDetails from './CommentDetails';
 import faker from 'faker';
 
-const ApprovalCard = () => {
+const ApprovalCard = ({children}) => {
     return (
         <div class="ui card">
-            <div className="content">
-                <CommentDetails 
+            <div className="content"> { children }
+                {/* <CommentDetails 
                     author={ faker.name.firstName() } 
                     avatar={ faker.image.avatar() } 
                     date={ faker.date.past().toDateString() } 
                     content={ faker.lorem.words(3) } 
-                />
+                /> */}
             </div>
             <div className="extra content">
                 <div className="ui two buttons">
